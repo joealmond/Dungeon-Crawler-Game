a100c03035a7ca9154e3fbdd80c361dd50746072
tmsnvkcc <127792799+tmsnvkcc@users.noreply.github.com>
2023-10-04 12:40:01 +0200

    player.java interaction updates;

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -44,15 +44,19 @@ public class Player extends Actor {
     }
 
     public void interactWithPlayer(Cell nextCell) {
-//        if (this.getCell().hasItem()) {
-//            this.getCell().getItem().interactWithPlayer(nextCell, currentHealth);
-//        }
-        handleCampfireInteraction(nextCell);
-        handleGoldenKeyInteraction(nextCell);
+        if (this.getCell().hasItem()) {
+            this.getCell().getItem().interactWithPlayer(nextCell);
+        }
+//        handleCampfireInteraction(nextCell);
+//        handleGoldenKeyInteraction(nextCell);
     }
 
     private void handleCampfireInteraction(Cell nextCell) {
-
+        if (checkIfNextCellHasGivenItem(nextCell, "campfire")) {
+            if (this.getCurrentHealth() < Player.MAX_HEALTH) {
+                this.currentHealth += 1;
+            }
+        }
     }
 
     private void handleGoldenKeyInteraction(Cell nextCell) {
